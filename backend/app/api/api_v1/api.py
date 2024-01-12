e94a4dc69b30dfe2a43fbf89c028177275767c5e
badbubble <liuhanjun369@gmail.com>
2024-01-12 14:39:24 +0100

    delete all complicated code

--- backend/app/api/api_v1/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.api_v1.endpoints import capsule
-
-api_router = APIRouter()
-api_router.include_router(capsule.router, prefix="/capsule", tags=["capsule"])
--- backend/app/models/code.py
+++
@@ -1,5 +0,0 @@
-from collections import namedtuple
-
-Response = namedtuple('Response', ['code', 'message'])
-
-Success = Response(1000, "Success")
