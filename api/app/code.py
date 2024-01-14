38566dbf1539243656a21323bc3380508559d5a2
badbubble <liuhanjun369@gmail.com>
2024-01-14 21:22:09 +0100

    add code about past date error

--- api/app/code.py
+++ api/app/code.py
@@ -9,5 +9,5 @@ param_error = Response(2000, "parameter error")
 param_title_error = Response(2001, f"the length of title is less than {settings.MIN_TITLE_LEN}")
 param_message_error = Response(2002, f"the length of message is less than {settings.MIN_TITLE_LEN}")
 param_expire_error = Response(2003, f"the open date is longer than {settings.MAX_EXPIRE_DAY}")
-
+param_date_invalid = Response(2004, f"the open date is smaller than the current date")
 server_error = Response(4000, f"server internal error")
