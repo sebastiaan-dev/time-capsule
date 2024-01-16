f0e98379a2f582ace84abcfa69ec631c5568ab37
badbubble <liuhanjun369@gmail.com>
2024-01-16 13:02:00 +0100

    1. add creating time. 2.add flag for next button. 3. fix datetime issue

--- api/main.py
+++ api/main.py
@@ -35,13 +35,14 @@ def get_db():
 
 @app.post("/timecapsule")
 async def create_capsule(item: schemas.CapsuleCreate, db: Session = Depends(get_db)):
+    item.date = item.date.replace(tzinfo=None)
     if item.title is None or len(item.title) < settings.MIN_TITLE_LEN:
         return {"code": param_title_error.code, "message": param_title_error.message, "data": None}
     if item.message is None or len(item.message) < settings.MIN_MESSAGE_LEN:
         return {"code": param_message_error.code, "message": param_message_error.message, "data": None}
-    if item.date - datetime.now(timezone.utc) > timedelta(days=settings.MAX_EXPIRE_DAY):
+    if item.date - datetime.now() > timedelta(days=settings.MAX_EXPIRE_DAY):
         return {"code": param_expire_error.code, "message": param_title_error.message, "data": None}
-    if item.date < datetime.now(timezone.utc):
+    if item.date < datetime.now():
         return {"code": param_date_invalid.code, "message": param_date_invalid.message, "data": None}
 
     result = crud.create_capsule(db=db, title=item.title, message=item.message, date=item.date)
@@ -51,17 +52,24 @@ async def create_capsule(item: schemas.CapsuleCreate, db: Session = Depends(get_
 
 @app.get("/timecapsules")
 async def get_capsule(start: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    
     capsules = crud.get_capsule(db, start, limit)
     if capsules is None:
         return {"code": server_error.code, "message": server_error.message, "data": []}
+    
+    # next flag will be true if the number of capsule is equal to limit
+    flag_next = False
+    if len(capsules) == limit:
+        flag_next = True
+
     opened_capsules = []
     closed_capsules = []
 
     for capsule in capsules:
-        if capsule.date < datetime.now():
+        if capsule.date <= datetime.now():
             # TODO: implement
-            capsule.createdAt = datetime.now()
-            capsule.openedAt = datetime.now()
+            capsule.createdAt = capsule.created_at
+            capsule.openedAt = capsule.date
             opened_capsules.append(capsule)
         else:
             capsule.title = None
@@ -70,14 +78,22 @@ async def get_capsule(start: int = 0, limit: int = 100, db: Session = Depends(ge
 
     opened_capsules.sort(key=lambda k: k.date, reverse=True)
     closed_capsules.sort(key=lambda k: k.date, reverse=False)
+
     total_opened_count, total_closed_count = crud.get_capsule_count(db)
-    closed_capsule = {
-        "next_time": closed_capsules[0].date,
-    }
+    
+    if len(closed_capsules) == 0:
+        closed_capsule = {
+            "next_time": None,
+        }
+    else:
+        closed_capsule = {
+            "next_time": closed_capsules[0].date,
+        }
 
     return {"code": success.code, "message": success.message, "data": {
         "opened_capsules": opened_capsules,
         "opened_count": total_opened_count,
         "closed_capsule": closed_capsule,
         "closed_count": total_closed_count,
+        "next_flag": flag_next,
         }}
